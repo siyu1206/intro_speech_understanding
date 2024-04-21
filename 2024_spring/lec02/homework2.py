23a2d8972decc9b01b10361368510d2f732692e1
siyu1206 <166461682+siyu1206@users.noreply.github.com>
2024-04-21 10:47:51 +0900

    i did homework2.py
    I have changed strings.

--- 2024_spring/lec02/homework2.py
+++ 2024_spring/lec02/homework2.py
@@ -12,3 +12,18 @@ h = None
 i = None
 j = None
 k = None
+
+
+a = "my name is siyu"
+b= 7%3
+c = 55*234
+name1 = "KCGI"
+name2 = "Kyoto"
+d = name1+'@'+name2
+e = round(3.458,2)
+f = str(57)
+g = 5==9
+h = str(-23.0)
+i = float(False)
+j = bool("Python")
+k = "global"+"_squ"
